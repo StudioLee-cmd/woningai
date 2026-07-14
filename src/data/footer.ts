140cda9568694484f8fac5a22db63e9a30a34ded
StudioLee-cmd <Tim@studiolee.nl>
2026-07-14 08:00:55 +0200

    footer: echte social-links (YouTube @DIGITAL.STUDIOLEE) i.p.v. placeholder instagram/linkedin (sites-social-links-messaging-fix)

--- src/data/footer.ts
+++ src/data/footer.ts
@@ -48,8 +48,7 @@ export const footerDetails: {
     email: 'tim@woningai.nl',
     telephone: '+31612345678',
     socials: {
-instagram: 'https://www.instagram.com',
-        linkedin: 'https://www.linkedin.com',
+        youtube: 'https://www.youtube.com/@DIGITAL.STUDIOLEE',
     },
     kvk: '98933353',
     btw: 'NL005362803B09',
